629e06f7d9720b5bade8371e2f0bd1c35f5ec32e
Dikshith Devadiga <dikshithdevadiga8@gmail.com>
2026-03-14 03:20:01 +0530

    Added homepage*

---
+++ tools/image-editor/main.js
@@ -0,0 +1,135 @@
+"use strict";
+
+
+const upload = document.querySelector("input[type='file']");
+
+
+document.querySelector("#upload-btn").addEventListener("click", () => {
+    upload.click();
+})
+document.querySelector("#drop").addEventListener("click", () => {
+    upload.click();
+})
+
+
+document.querySelector("#crop_resize_expand").addEventListener("click", () => {
+    document.querySelector("#crop-resize-option").classList.toggle("show");
+    document.querySelector("#crop_resize_expand svg").classList.toggle("rotate");
+})
+document.querySelector("#transform-expand").addEventListener("click", () => {
+    document.querySelector("#transform-option").classList.toggle("show");
+    document.querySelector("#transform-expand svg").classList.toggle("rotate")
+
+})
+
+//canvas 
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+let imgRef;
+
+canvas.width = 300;
+canvas.height = 150;
+
+
+function drawDefaultCanvas() {
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+
+drawDefaultCanvas();
+
+function drawImage(file) {
+    document.querySelector("#upload-btn").style.pointerEvents = "none";
+    document.querySelector("#dropzone").style.display = "none";
+    displayFileDetail(file);
+
+    //main part
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        const img = new Image();
+        img.onload = () => {
+            imgRef = img;
+            canvas.width = img.naturalWidth;
+            canvas.height = img.naturalHeight;
+            ctx.imageSmoothingEnabled = true;
+            ctx.imageSmoothingQuality = "high";
+            ctx.drawImage(img, 0, 0);
+            document.querySelector("#upload-btn").style.pointerEvents = "";
+        }
+        img.onerror = () => {
+            console.erro("Failed to load image");
+            document.querySelector("#upload-btn").style.pointerEvents = "";
+
+        }
+        img.src = e.target.result;
+    }
+    reader.readAsDataURL(file);
+
+
+
+}
+
+
+
+document.querySelector("input[type='file']").addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    if (!file.type.startsWith("image/")) {
+        console.error("Upload image");
+        return;
+    }
+    drawImage(e.target.files[0]);
+})
+
+
+
+
+function displayFileDetail(file) {
+    if (document.querySelector("#display-file-detail")) {
+        document.querySelector("#display-file-detail").remove();
+    }
+    const div = document.createElement("div");
+    div.id = "display-file-detail";
+    const h1 = document.createElement("h1");
+    h1.textContent = `Name: ${file.name}`;
+    const p1 = document.createElement("p");
+    p1.textContent = `Type: ${file.type}`;
+    const p2 = document.createElement("p");
+    p2.textContent = `Size: ${(file.size / 1024).toFixed(1)}KB`;
+
+    div.appendChild(h1);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    document.querySelectorAll("#left-header p")[1].innerHTML = "";
+    document.querySelector("#left-header").insertAdjacentElement("afterend", div);
+}
+
+
+
+
+
+//drag and drop
+const dragzone = document.querySelector("#drop");
+dragzone.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    dragzone.classList.add("active");
+})
+dragzone.addEventListener("dragleave", (e) => {
+    if (!dragzone.contains(e.relatedTarget)) {
+        dragzone.classList.remove("active");
+    }
+})
+dragzone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+})
+dragzone.addEventListener("drop", (e) => {
+    e.preventDefault();
+    dragzone.classList.remove("active");
+    const file = e.dataTransfer.files[0];
+    if (!file.type.startsWith("image/")) {
+        console.error("Upload image");
+    }
+    drawImage(file);
+})
